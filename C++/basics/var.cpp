f7f6fee96fcfb14cb5be2b7cf719522e55b44692
Kavyan Nayak <kavyannayak104@gmail.com>
2025-12-11 19:48:52 +0530

    first commit

---
+++ C++/basics/var.cpp
@@ -0,0 +1,23 @@
+// int , char , float, double, long , string , bool
+
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int Mynum = 15;
+    char Letter = 'R';
+    float point = 5.99;
+    string MyText = "Hello";
+    bool myBool = "true";
+
+    cout << "Integer :- " << Mynum << endl;
+    cout << "Character :- " << Letter << endl;
+    cout << "Float :- " << point << endl;
+    cout << "String :- " << MyText << endl;
+    cout << "Boolean :- " << myBool << endl;
+
+}
